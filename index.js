6d3aa897d9d810da0fce372d0dc88e6d8a5a278d
carboni09 <caleb.adegbite@hotmail.com>
2018-02-19 21:12:17 +0100

    another commit

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ let bodyParser = require('body-parser')
 let http = require('http')
 let request = require('request')
 let cheerio = require('cheerio')
+let firebase = require('firebase/app')
+               require('firebase/firestore')
+
+    let config = {
+    apiKey: "AIzaSyBmkcuv__YHDjIqZtaO8Or7tVAWmkxRE58",
+    authDomain: "phonebotalpha.firebaseapp.com",
+    databaseURL: "https://phonebotalpha.firebaseio.com",
+    projectId: "phonebotalpha",
+    storageBucket: "phonebotalpha.appspot.com",
+    messagingSenderId: "661337425451"
+    }
 
 const server = express()
 
@@ -12,11 +23,15 @@ server.use(bodyParser.urlencoded({
 
 server.use(bodyParser.json())
 
+firebase.initializeApp(config);
+
 server.get('/', function (req, res) {
+    
     res.send('S\'up')
+    
 })
 
-server.post('/search-phones/:phone', function(req,res) {
+server.post('/save-phones/:phone', function(req,res) {
     let info = ''
     let phone_to_search = req.params.phone;
     console.log(req.body.result)
@@ -41,18 +56,43 @@ server.post('/search-phones/:phone', function(req,res) {
                     
                 };
       
-                data.push(metadata)
-                  
+                //data.push(metadata)
+                firebase.firestore().collection('phones').doc(metadata.phone).set(metadata)
+                    .then( doc => {
+                        console.log(doc.id)
+                    }).then( function () {
+                        res.send('Done')
+                        return 'Done did it'
+                    })
               });
              
-              data = JSON.stringify(data)
-              console.log(data)
-              res.send(data)
+             // data = JSON.stringify(data)
+            //  console.log(data)
+               
+ 
+             // res.send(metadata)
               //console.log(data)
              
           }
+         // console.log(data[0])
+         
 })
 
+
+})
+
+
+
+server.post('/find-phones/:phone', function (req, res) {
+    let phone_to_find = req.params.phone;
+    phone_to_find = phone_to_find.replace(/\+/g, " ")
+    console.log('Find '+phone_to_find)
+    firebase.firestore().collection('phones').doc(phone_to_find).get()
+        .then( doc => {
+            console.log(  )
+            res.send(doc.data())
+            return
+        })
 })
 
 server.listen((process.env.PORT || 8011 ), function() {
